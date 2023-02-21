3014fed5fd0ea3f230925e746738a067d42d5cdb
mayud <mayudong200333@gmail.com>
2023-02-21 14:32:31 +0900

    added validate data for this project

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Get,Post,Body,Param } from '@nestjs/common';
+import {CreateMessageDto} from './dtos/create-message.dto';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+    @Get()
+    listMessages(){
+
+    }
+
+    @Post()
+    createMessage(@Body() body:CreateMessageDto){
+        console.log(body);
+    }
+
+    @Get('/:id')
+    getMessages(@Param('id') id:string){
+        console.log(id)
+    }
+}
